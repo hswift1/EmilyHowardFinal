5c76aa1caf217e0d29aa3a3e5b0795592a36ef26
Soft Student <softdevstudent@robotics-mba-04.local>
2019-01-18 10:51:31 -0500

    Initial Commit

---
+++ EmilyHowardFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmilyHowardFinal
+//
+//  Created by Soft Dev Student on 1/18/19.
+//  Copyright © 2019 Emily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
